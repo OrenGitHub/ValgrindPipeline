b6c18948a3cd3d17f7c13e197e5ffefc152aa909
OrenGitHub <tuna.is.good.for.you@gmail.com>
2022-11-05 13:09:09 +0200

    make example smaller

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,10 @@ class Address { public:
     Address(const std::string &s):street(s), building(6){}
 };
 
-class Person { public:
-    Address *address = nullptr;
-    std::string name;
-    Person(Address *address_,const std::string &name_)
-        :address(address_), name(name_){}
-};
-
 int main(int argc, char **argv)
 {
     Address *address = new Address("5thAvenue");
-    Person *person = new Person(address, "moish");
-    person->address->building = 20;
+    address->building = 20;
     std::cout << "OK PERSON\n";
     return 0;
 }
